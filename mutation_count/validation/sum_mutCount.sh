96f83cc1f57682e76db66c768fd26c722b2810f0
im-ant <anthony.gx.chen@gmail.com>
2018-01-10 15:46:20 +0000

    Script that ouputs the number of variant per gene, given a variant file

---
+++ mutation_count/validation/sum_mutCount.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+# ============================================================================
+# Script that takes a variant file and outputs the number of variants per gene
+#   (see below for assumptions made on formatting), as well as the total
+#   number of variants for the entire file
+#
+# Input   $1 - .csv variant file with integer mutations
+# Output: Printed each gene and the mutation sum for it
+#
+# Notes on assumptions:
+#   - The input file has:
+#       - individuals variants starting on column 184 with 6 col step size
+#       - The gene column is column 10
+#       - The detailed annotation column is column 11
+# ============================================================================
+
+#Variable for the input variant file
+VAR_FILE=$1
+
+#Create a temporary file to store the genes and the mutation count per row
+temp_file=`mktemp`
+
+#Use awk to count the mutations
+awk -F, '
+    # for every row, calculate the sum
+    {sum = 0; for (i=184; i<=NF; i+=6) sum += $i}
+
+    # print out the mutation sum with the gene and annotation
+    {print $10","$11","sum}
+' $VAR_FILE > $temp_file
+
+#Use awk to count the number of mutations
+awk -F, '
+  {a[$1]+=$3}
+  END{for(i in a) print i","a[i]}
+' $temp_file
+
+echo
+awk -F, '
+  {sum+=$3}
+  END{print "Overall Mutation Sum =",sum}
+' $temp_file
+
+#Remove the temp file
+rm $temp_file
