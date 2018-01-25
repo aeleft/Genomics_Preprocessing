e2923d82a0f821184fe9574e05f98e153c13de9d
im-ant <anthony.gx.chen@gmail.com>
2018-01-25 15:07:31 +0000

    Changed sed script such that it takes up less space and only iterates thorugh the stream once for string replacement

--- mutation_count/validation/genotype_2_mutCount.sh
+++ mutation_count/validation/genotype_2_mutCount.sh
@@ -10,32 +10,25 @@
 # Output: .output file with the string sequences replaced
 #
 # Note that the `sed` function assumes GNU sed (i.e. Linux server). It will
-# not work if run using OS/X
+# not work if run using OS/X. Also that the stdout is appended to the output
+# stream but does not explicitly overwrites it.
 # ============================================================================
 
 #Set up path variables
 VAR_FILE=$1
 OUTPUT_PATH=$2
 
-echo "Initializing file $OUTPUT_PATH"
-#Create a copy of the input file as the output file
-cp $VAR_FILE $OUTPUT_PATH
+
+#Start sed script to replace various phenotypes
+sed '
+	s/Filtered Heterozygote/1/g;
+	s/Heterozygote/1/g;
+
+	s/Filtered Homozygote/2/g;
+	s/Homozygote/2/g;
+	
+	s/Filtered Trizygote/3/g;
+	s/Trizygote/3/g
+' $VAR_FILE >> $OUTPUT_PATH
 
 
-### Carry out in-place replacement for the output file ###
-#Note, the "Filtered [genotype]" conversion needs to be done first
-
-#Filtering the heterozygote
-sed -i 's/Filtered Heterozygote/1/g' $OUTPUT_PATH
-sed -i 's/Heterozygote/1/g' $OUTPUT_PATH
-
-#Filtering the Homozygote
-sed -i 's/Filtered Homozygote/2/g' $OUTPUT_PATH
-sed -i 's/Homozygote/2/g' $OUTPUT_PATH
-
-#Filtering the trizygote
-sed -i 's/Filtered Trizygote/3/g' $OUTPUT_PATH
-sed -i 's/Trizygote/3/g' $OUTPUT_PATH
-
-
-echo "Done!"
